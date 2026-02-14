fda6f980a7bd6f3bf89466392121d87177b4849c
Somesh Eskala <somesh.eskala@oracle.com>
2026-02-14 16:11:24 -0600

    Configure Supabase realtime

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  SUPABASE_URL: "",
-  SUPABASE_ANON_KEY: "",
+  SUPABASE_URL: "https://exviddyempafktiykswe.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV4dmlkZHllbXBhZmt0aXlrc3dlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEwODkyMTEsImV4cCI6MjA4NjY2NTIxMX0.KAmr6nT2pqxTKO3uRr4CLSxEVYkUWLEnndabFdCczCA",
   SHARED_STATE_ROW_ID: "shared"
 };
